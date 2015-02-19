d87c7b43b3c26143f1e3900a52f912614f25d112
Andreas Fransson <andreas@enigma.cc>
2015-02-19 15:20:56 +0100

    subclass singleton of AFHTTPSessionManager

---
+++ app/shared/st_http_session_manager.rb
@@ -0,0 +1,18 @@
+class STHTTPSessionManager < AFHTTPSessionManager
+	# make this class a singleton
+	def self.setup!
+		Dispatch.once { @instance ||= new }
+		@instance
+	end
+
+	def self.instance
+		@instance
+	end
+	# end make singleton
+
+	def init
+		base_url = 'http://shorti.org/api/v1/'
+		if initWithBaseURL(NSURL.URLWithString(base_url));end
+		self
+	end
+end
