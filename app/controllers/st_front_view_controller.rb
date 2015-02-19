a68825b1770b8e23abfb2a35c986140ff4dd3004
Andreas Fransson <andreas@enigma.cc>
2015-02-19 13:28:44 +0100

    adds tapped event handler to label

--- app/controllers/st_front_view_controller.rb
+++ app/controllers/st_front_view_controller.rb
@@ -10,4 +10,10 @@ class STFrontViewController < UIViewController
 		# The rect here must fill the entire screen
 		self.view = STFrontView.alloc.initWithFrame(CGRectMake(0, 0, Device.screen.width, Device.screen.height))
 	end
+
+	def viewDidLoad
+		view.label.when_tapped do
+			puts 'ping'
+		end
+	end
 end
--- app/views/st_front_view.rb
+++ app/views/st_front_view.rb
@@ -1,4 +1,6 @@
 class STFrontView < UIView
+	attr_reader :label
+
 	def initWithFrame(rect)
 		if super
 			self.backgroundColor = UIColor.whiteColor
@@ -6,15 +8,15 @@ class STFrontView < UIView
 			# Create subview which spans the inner bounds of the navigation view
 			contentView = UIView.alloc.initWithFrame(App.delegate.navigation_view_rect)
 
-			label = UILabel.alloc.initWithFrame(CGRectZero)
-			label.text = 'My label'
-			label.textColor = UIColor.grayColor
-			label.sizeToFit
+			@label = UILabel.alloc.initWithFrame(CGRectZero)
+			@label.text = 'My label'
+			@label.textColor = UIColor.grayColor
+			@label.sizeToFit
 
 			# Position label on center of screen
-			App.delegate.center_view(label, contentView)
+			App.delegate.center_view(@label, contentView)
 
-			contentView.addSubview(label)
+			contentView.addSubview(@label)
 			addSubview(contentView)
 		end
 		self
