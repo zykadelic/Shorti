9eee1710d0e955022543d777fd814e0d1bc3eea6
Andreas Fransson <andreas@enigma.cc>
2015-02-19 21:24:41 +0100

    missed line for last commit

--- app/models/st_api.rb
+++ app/models/st_api.rb
@@ -1,6 +1,6 @@
 class STAPI
 	# TODO Refactor response to separate response class
-	def self.get_daily(parameters = {}, &block)
+	def self.get_daily(parameters = {}, block)
 		STHTTPSessionManager.instance.GET('daily', parameters:parameters, success:lambda do |task, response|
 			block.call({ error: false, data: response['daily_shortis'] })
 		end, failure:lambda do |task, error|
