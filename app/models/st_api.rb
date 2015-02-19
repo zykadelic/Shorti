a4d4a0914d5102d2860813ec56dda9cf5f2bd779
Andreas Fransson <andreas@enigma.cc>
2015-02-19 21:36:50 +0100

    parameters are unnecessary here

--- app/models/st_api.rb
+++ app/models/st_api.rb
@@ -1,7 +1,7 @@
 class STAPI
 	# TODO Refactor response to separate response class
-	def self.get_daily_channel(parameters = {}, block)
-		STHTTPSessionManager.instance.GET('channels/dGtvt0Y', parameters:parameters, success:lambda do |task, response|
+	def self.get_daily_channel(block)
+		STHTTPSessionManager.instance.GET('channels/dGtvt0Y', parameters:nil, success:lambda do |task, response|
 			block.call({ error: false, data: response['channel'] })
 		end, failure:lambda do |task, error|
 			block.call({ error: true, error_message: error })
