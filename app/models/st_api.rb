f2eb3cd47fa3db65683b77c2b6244158e1e66bc1
Andreas Fransson <andreas@enigma.cc>
2015-02-23 00:33:22 +0100

    move currentChannel to separate method

--- app/models/st_api.rb
+++ app/models/st_api.rb
@@ -1,13 +1,19 @@
 class STAPI
 	# TODO Refactor response to separate response class
 	def self.getCurrentChannel(block)
-		default = 'dGtvt0Y' # FIXME Add this to some sort of configatron
-		channel = NSUserDefaults.standardUserDefaults['channel'] || default
-
-		STHTTPSessionManager.instance.GET("channels/#{channel}", parameters: nil, success: lambda do |task, data|
+		STHTTPSessionManager.instance.GET("channels/#{currentChannel}", parameters: nil, success: lambda do |task, data|
 			block.call({ error: false, data: data })
 		end, failure: lambda do |task, error|
 			block.call({ error: true, error_message: error })
 		end)
+	end
+
+	def self.currentChannel
+		@currentChannel ||= currentChannelFromSettings
+	end
+
+	def self.currentChannelFromSettings
+		default = 'dGtvt0Y' # FIXME Add this to some sort of configatron
+		return NSUserDefaults.standardUserDefaults['channel'] || default
 	end
 end
